3738e357d8a47684cfd210e39b0d2f933acc00f9
Alfian Losari <alfianlosari@gmail.com>
2020-01-27 10:18:53 +0700

    Build ajustment

--- MovieSwiftUI/Resources/SceneDelegate.swift
+++ MovieSwiftUI/Resources/SceneDelegate.swift
@@ -13,7 +13,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -21,13 +20,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Use a UIHostingController as window root view controller
         let movieStore = MovieStore.shared
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIHostingController(rootView: Home().environmentObject(MovieHomeData(movieService: movieStore, endpoints: Endpoint.allCases))
+        let rootView = Home()
+            .environmentObject(MovieHomeData(movieService: movieStore, endpoints: Endpoint.allCases))
             .environmentObject(MovieSearchData(movieService: movieStore))
             .environmentObject(KeyboardData())
-        )
-        self.window = window
-        window.makeKeyAndVisible()
+        
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: rootView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
